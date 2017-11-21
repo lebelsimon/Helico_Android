92894cd549f192ccc14f519412f0b4f23c8cf29c
Porosim <simon.lebel17@gmail.com>
2017-11-21 16:10:15 +0100

    Init Depot

---
+++ Helico/app/src/main/java/com/example/simon/helico_flap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.simon.helico_flap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
